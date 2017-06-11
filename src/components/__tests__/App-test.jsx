23b458138f0862f7842cb58161dffe16c41671ab
Eric <eric.tillberg@gmail.com>
2017-06-11 08:38:51 -0400

    Add react-test-renderer and chat test

--- src/components/__tests__/App-test.jsx
+++ src/components/__tests__/App-test.jsx
@@ -3,18 +3,34 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import App from '../App';
 
+import Chat from '../Chat';
 import Matchmaker from '../Matchmaker';
 
 describe('App', () => {
   const component = shallow(<App />);
 
   describe('before chatting', () => {
+    beforeEach(() => {
+      component.setState({
+        inChat: false
+      });
+    });
+
     it('renders a matchmaker', () => {
       expect(component.find(Matchmaker).length).to.equal(1);
     });
   });
 
   describe('while chatting', () => {
+    beforeEach(() => {
+      component.setState({
+        inChat: true,
+        uid: '123',
+      });
+    });
 
+    it('renders a chat', () => {
+      expect(component.find(Chat).length).to.equal(1);
+    });
   });
 });
