83b62016e1b3df1317f0cce25400cf65d18074cb
Eric <eric.tillberg@gmail.com>
2017-07-15 21:39:45 -0400

    Test and eslint setup

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,15 @@
 var sinon = require('sinon');
 require('babel-register')();
-var jsdom = require('jsdom').jsdom;
+var mount = require('enzyme').mount;
+var shallow = require('enzyme').shallow;
+var React = require('react');
+var Firebase = require('firebase');
+var chai = require('chai');
 
+global.expect = chai.expect;
 global.sinon = sinon;
+global.React = React;
+global.shallow = shallow;
+global.mount = mount;
+global.Firebase = Firebase;
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,29 @@ const path = require('path');
 module.exports = {
   entry: './src/main.jsx',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'build/'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
+        use: {
+          loader: 'babel-loader',
+					options: {
+						presets: ['es2015', 'react']
+					}
+        },
       },
     ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: {index: '/index.html'},
+    contentBase: './',
   },
 };
